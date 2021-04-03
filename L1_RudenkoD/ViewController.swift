2f395e3c63c667cc2862cb7ec8d6e3cc00ab2cbf
Dmitry Rudenko <dirudenko@icloud.com>
2021-04-03 19:47:18 +0300

    Initial Commit

---
+++ L1_RudenkoD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L1_RudenkoD
+//
+//  Created by Dmitry on 03.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
