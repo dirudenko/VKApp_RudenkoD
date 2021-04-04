7a9f6f87dddd728466e12fbebe18d6a50da084ce
Dmitry Rudenko <dirudenko@icloud.com>
2021-04-04 13:05:07 +0300

    v1

---
+++ L1_RudenkoD/LoginFormController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginFormController.swift
+//  L1_RudenkoD
+//
+//  Created by Dmitry on 03.04.2021.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+  @IBAction func loginButton(_ sender: Any) {
+  }
+  
+  
+  @IBOutlet weak var loginInput: UITextField!
+  
+  @IBOutlet weak var passwordInput: UITextField!
+  
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
