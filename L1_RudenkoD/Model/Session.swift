dfc1116d298bcedda84cac47ff37261f52e357e1
Dmitry Rudenko <dirudenko@icloud.com>
2021-05-21 22:56:09 +0300

    L1 Complete

---
+++ L1_RudenkoD/Model/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  VKapp_RudenkoD
+//
+//  Created by Dmitry on 21.05.2021.
+//
+
+import Foundation
+
+final class Session: NSObject {
+  static let shared = Session()
+  private override init() {
+    super.init()
+  }
+  
+  var token = String()
+  var userId = Int()
+}
